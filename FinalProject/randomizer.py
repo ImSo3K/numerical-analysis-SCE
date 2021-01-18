69d596dedce3f6e2e85018f2968f5ddb48ff78da
Sharon Yaroshetsky <imso3k@gmail.com>
2021-01-18 13:58:46 +0200

    question randomizer

---
+++ FinalProject/randomizer.py
@@ -0,0 +1,19 @@
+from random import randint, choice, randrange
+
+id = ['313526675', '321967556', '203192331', '207948381']
+questions = []
+ranges = [(1, 9), (10, 17), (18, 29), (18, 29), (30, 35), (36, 41)]
+for _ in range(6):
+  rand_id = choice(id)
+  rand_digit = int(rand_id[randint(1, 8)])
+
+  if len(ranges) == 1:
+    single_range = ranges.pop(0)
+  elif rand_digit % 2 == 0:
+    single_range = ranges.pop(randrange(0, len(ranges), 2))
+  else:
+    single_range = ranges.pop(randrange(1, len(ranges), 2))
+  
+  questions.append(randint(single_range[0], single_range[1]))
+
+print(questions)
